a63b4451c4d73e2648ef84a6d716018ef4691ddc
wscott <wscott0318@gmail.com>
2023-01-17 20:16:55 +0200

    Update

--- helpers/config.ts
+++ helpers/config.ts
@@ -1,4 +1,4 @@
 export const Config = {
-  apiUrl: process.env.API_URL ?? 'http://localhost:3001/graphql',
-  appName: process.env.APP_NAME ?? 'CyberSafely',
+  apiUrl: process.env.NEXT_PUBLIC_API_URL ?? 'http://localhost:3001/graphql',
+  appName: process.env.NEXT_PUBLIC_APP_NAME ?? 'CyberSafely',
 }
