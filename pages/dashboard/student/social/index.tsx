57fdfabb31501269f2bfc81c95dfa92a49955c44
wscott <wscott0318@gmail.com>
2023-05-31 11:21:11 +0300

    Remove social banner

--- pages/dashboard/student/social/index.tsx
+++ pages/dashboard/student/social/index.tsx
@@ -1,24 +1,15 @@
-import { Alert, Grid, Stack, Typography } from '@mui/material'
-import { useRouter } from 'next/router'
+import { Grid, Typography } from '@mui/material'
 import { withDashboardLayout } from '../../../../components/dashboard/Layout'
 import { SocialButtonConfig } from '../../../../components/forms/UpdateUserForm'
 import { useUser } from '../../../../utils/context/auth'
 
 function Social() {
-  const router = useRouter()
-
   const { user, refetchUser } = useUser()
 
   return (
     <Grid container spacing={2}>
       <Grid item xs={12}>
-        <Stack flexGrow={1} spacing={1}>
-          <Typography variant="h5">Social Networks</Typography>
-          <Alert severity="info">
-            Right now we only support Twitter, Facebook but more social integrations are rolling out weekly, please
-            check back for additions.
-          </Alert>
-        </Stack>
+        <Typography variant="h5">Social Networks</Typography>
       </Grid>
       <Grid item xs={12} sm={4} md={3}>
         <SocialButtonConfig name="TWITTER" user={user} refetch={refetchUser} />
