31ae1912b9e30b2e75953148012919c0326ee473
Toko <tokomostwanted@gmail.com>
2023-08-10 14:33:11 +0400

    Add posts table to students + detailed post view

---
+++ pages/dashboard/student/posts/[postId]/index.tsx
@@ -0,0 +1,21 @@
+import { GetServerSideProps } from 'next'
+import { withDashboardLayout } from '../../../../../components/dashboard/Layout'
+import { DetailedPostView } from '../../../../../components/shared/DetailedPostView'
+
+type Props = {
+  postId: string
+}
+
+function Post({ postId }: Props) {
+  return <DetailedPostView postId={postId} backURL="/dashboard/student/posts" />
+}
+
+export const getServerSideProps: GetServerSideProps<Props> = async (ctx) => {
+  const postId = ctx.params!.postId as string
+  return { props: { postId } }
+}
+
+export default withDashboardLayout(Post, {
+  title: 'Post',
+  maxWidth: 'md',
+})
